8a5a290fb06604514970a8b611586fd204776b0a
Blue-pill-786 <113605539+Blue-pill-786@users.noreply.github.com>
2023-11-03 03:38:16 +0530

    Update ToDoRoutes.js

--- routes/ToDoRoutes.js
+++ routes/ToDoRoutes.js
@@ -4,7 +4,7 @@ const {getToDos, saveToDos, updateToDos, deleteToDos} = require("../controller/T
 
 const router = Router();
 router.get("/",(req,res)=>{
-    res.send("<h1>Welcome To My Todo App</h1>")
+    res.send("<h1>Welcome To My Todo App Working</h1>")
 })
 router.get("/get", getToDos);
 router.post("/save", saveToDos);
